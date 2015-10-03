81017627f41ac8d075dde3777e0024e971ab4ab0
Thomas Richner <mail@trichner.ch>
2015-10-03 19:39:52 +0200

    fancier log

--- src/log.py
+++ src/log.py
@@ -1,9 +1,48 @@
 import logging
 
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#The background is set with 40 plus the number of the color, and the foreground with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': MAGENTA,
+    'ERROR': RED
+}
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
 def new_logger(name,loglevel=logging.WARN):
     logger = logging.getLogger(name)
     hdlr = logging.FileHandler(name + '.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    FORMAT = "[$BOLD%(name)-8s$RESET][%(levelname)-18s]  %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+    formatter = ColoredFormatter(COLOR_FORMAT)
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
     logger.setLevel(loglevel)

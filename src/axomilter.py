2cf58a69ae7d07e06b6cecf1da0b00bf4312ebdb
Thomas Richner <mail@trichner.ch>
2015-10-03 15:32:53 +0200

    removed axolol

--- src/axomilter.py
+++ src/axomilter.py
@@ -5,7 +5,7 @@
 import libmilter as lm
 import sys
 import time
-from axoctl import *
+#from axoctl import *
 
 HOST = "example.com"
 
@@ -114,7 +114,7 @@ class AxoMilter(lm.ForkMixin, lm.MilterProtocol):
             elif self.rq_axo:
                 self.log("AXONAUT - axorq -> encrypt!")
                 # Encrypt dat shit!
-                AxoCtl().process_outbound(mail)
+                #AxoCtl().process_outbound(mail)
                 # axoctl.process_outbound(mail)
                 action = lm.DISCARD
             else:

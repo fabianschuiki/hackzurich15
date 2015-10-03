aaad12a81888c9f59004a2ff968d1f6a4470e8c6
Thomas Richner <mail@trichner.ch>
2015-10-03 22:31:13 +0200

    made template fancy

--- src/drunkenbishop/__init__.py
+++ src/drunkenbishop/__init__.py
@@ -9,21 +9,31 @@ def GetFPrintMail(mykey, myid, otherkey, otherid):
     otherkey_hash_str = hashlib.sha256(otherkey).hexdigest().upper()
     otherkey_hash_list = [int(otherkey_hash_str[i:i+8], 16) for i in range(0,len(otherkey_hash_str), 8)]
 
-    mykey_bishop = str(Fingerprint(mykey_hash_list, myid.split("@")[0], "SHA256"))
-    otherkey_bishop = str(Fingerprint(otherkey_hash_list, otherid.split("@")[0], "SHA256"))
+    my_name = myid.split("@")[0]
+    other_name = otherid.split('@')[0]
 
+    mykey_bishop = str(Fingerprint(mykey_hash_list, my_name, "SHA256"))
+    otherkey_bishop = str(Fingerprint(otherkey_hash_list, other_name, "SHA256"))
 
-    mailtext = """Hello %s
-    Your e-mail conversation with %s was encrypted via the Axonaut e-mail
-    encryption service. To guarantee the security of your messages, compare
-    the following hashes with your partner %s through a secure second
-    channel. If the keys do not match, it is likely that your are victim of
-    a Man-in-the-Middle attack.
+    mail_template =     "Hello %(my_name)s,\n" + \
+                        "\n" + \
+                        "Your e-mail conversation with %(other_id)s was encrypted via the awesome\n" + \
+                        "Axonaut e-mail encryption service. To verify the identity of %(other_name)s,\n" + \
+                        "compare the following fingerprints with your partner.\n" + \
+                        "\n" + \
+                        "If they don't match you might be a victim to an active attack!\n"
 
-    Hash of your key:
-    %s
+    mailtext = mail_template % {'my_name': my_name,'other_name': other_name, 'my_id': myid, 'other_id': otherid}
+    # Append dat bishop
 
-    Hash of your partners key
-    %s""" % (myid, otherid, otherid, mykey_bishop, otherkey_bishop)
+    mykey_lines = mykey_bishop.splitlines()
+    otherkey_lines = otherkey_bishop.splitlines()
+    lines = zip(mykey_lines,otherkey_lines)
+
+    fingerprints = "\n"
+    for l1,l2 in lines:
+        fingerprints + ("%s %s\n" % (l1,l2))
+
+    mailtext += fingerprints
 
     return mailtext
